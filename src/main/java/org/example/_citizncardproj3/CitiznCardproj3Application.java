1778ba6950a5fd23377b2d76297b538f9aa3f353
alfo0924 <halo0924@yahoo.com.tw>
2024-10-31 11:03:38 +0800

    v1.01 init

---
+++ src/main/java/org/example/_citizncardproj3/CitiznCardproj3Application.java
@@ -0,0 +1,13 @@
+package org.example._citizncardproj3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiznCardproj3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiznCardproj3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/_citizncardproj3/CitiznCardproj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._citizncardproj3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiznCardproj3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
